275e2ca3d1b6d6af0831bc8663a187acfda31f5b
Nayna Pashilkar <nayna.pashilkar@gmail.com>
2017-06-06 18:09:23 +0000

    met gala lab

--- invitation.rb
+++ invitation.rb
@@ -1,1 +1,3 @@
-#Code Here!
+#Code Here
+gala_invitation = "The Museum of Modern Art invites you to their annual gala on Sunday the 22nd of May 2015. Festivities will be held at the MoMA at 11 W 53rd St, New York, NY 10019. See you then!"
+puts gala_invitation.gsub("Museum of Modern Art", "Metropolitan Museum").gsub("Sunday the 22nd of May 2015", "Saturday the 13th of May 2017")
--- metgala_invitation.rb
+++ metgala_invitation.rb
@@ -1,3 +1,7 @@
 #Code Here!
 
 gala_invitation = "The Museum of Modern Art invites you to their annual gala on Sunday the 22nd of May 2015. Festivities will be held at the MoMA at 11 W 53rd St, New York, NY 10019. See you then!"
+day = "Friday"
+date = "26th"
+year = "2017"
+puts gala_invitation = "The Museum of Modern Art invites you to their annual gala on #{day} the #{date} of May #{year}. Festivities will be held at the MoMA at 11 W 53rd St, New York, NY 10019. See you then!"
